618faa3912816a5001e538197bc21b8eee115133
MichaelBrandonFalk <102083222+MichaelBrandonFalk@users.noreply.github.com>
2026-04-14 12:51:12 -0700

    Revert "Add s3Organizer desktop apps"
    This reverts commit 4d99c5df1618f04dbe27b206fc399b240bb3ccae.

--- s3_copy_desktop_app/__init__.py
+++
@@ -1,1 +0,0 @@
-"""S3 copy desktop app package."""
--- s3_copy_desktop_app_launcher.py
+++
@@ -1,6 +0,0 @@
-"""Launcher script for PyInstaller packaging."""
-
-from s3_copy_desktop_app.app import main
-
-if __name__ == "__main__":
-    main()
